a3fada50fdd8dc1c26f1905a0636023f5ac4784b
Sylvain Girard <girard@phimeca.com>
2017-02-09 16:45:01 +0100

    Correct input variables in the bil100 example

--- otfmi/example/bil100.py
+++ otfmi/example/bil100.py
@@ -26,24 +26,16 @@ from pyfmi.fmi import FMUException
 import sys
 
 #§ 1. FMU model
-inputs_fmu = ["sinkQPUR1.T0", "sinkQPUR2.T0",
-              "sinkQPUR3.T0", "sinkQPUR4.T0",
-              "sourcePQARE.P0", "sourcePQARE.Q0",
-              "sourcePQARE.T0",
-              "sinkQGSS.Q0", "sinkQGSS.T0",
-              "sinkQGRE.Q0", "sinkQGRE.T0",
-              "sinkQSTR.T0"]
-
-inputs_fmu = ["sourcePQARE.P0", "sourcePQARE.Q0", "sourcePQARE.T0",
-              "sinkQGSS.Q0", "sinkQGRE.Q0"]
-
-outputs_fmu = ["BIL100.W_BIL100.signal"]
-
-def instantiate_model(with_initialization_script=False):
+def instantiate_model(inputs_fmu, outputs_fmu,
+                      with_initialization_script=False):
     """Instantiate an FMUFunction and set number of cores to use.
 
     Parameters:
     ----------
+    inputs_fmu : Sequence of strings, FMU input variable names.
+
+    outpus_fmu : Sequence of strings, FMU output variable names.
+
     with_initialization_script : Boolean, wether or not to use an
     initialization script. If not (default), appropriate start values are
     hardcoded in the FMU.
@@ -74,48 +66,20 @@ def instantiate_model(with_initialization_script=False):
 #§ 2. Random vector definition
 # Create the marginal distributions of the input random vector
 dict_distribution = {
-    "sinkQPUR1.T0":ot.Normal(500.2497543, 3.0),
-    "sinkQPUR2.T0":ot.Normal(500.2497543, 3.0),
-    "sinkQPUR3.T0":ot.Normal(500.2497543, 3.0),
-    "sinkQPUR4.T0":ot.Normal(500.2497543, 3.0),
-    "sourcePQARE.P0":ot.Normal(7032392.1, 20700.0),
-    "sourcePQARE.Q0":ot.Normal(2142.972222, 8.5),
-    "sourcePQARE.T0":ot.Normal(500.2497543, 3.0),
-    "sinkQGSS.Q0":ot.Normal(183.713226, 0.75),
-    "sinkQGSS.T0":ot.Normal(552.6833333, 3.2),
-    "sinkQGRE.Q0":ot.Normal(1922.331218, 8.5),
-    "sinkQGRE.T0":ot.Normal(552.6833333, 3.2),
-    "sinkQSTR.T0":ot.Normal(552.6833333, 3.2)
+    "PARE0":ot.Normal(7032392.1, 20700.0),
+    "QARE0":ot.Normal(2142.972222, 8.5),
+    "TARE0":ot.Normal(500.2497543, 3.0),
+    "QGSS0":ot.Normal(183.713226, 0.75),
+    "QGRE0":ot.Normal(1922.331218, 8.5),
+    "PGCT0":ot.Normal(6.54e6, 6.40e4),
 }
 
-dict_distribution = {
-    "sourcePQARE.P0":ot.Normal(7032392.1, 20700.0),
-    "sourcePQARE.Q0":ot.Normal(2142.972222, 8.5),
-    "sourcePQARE.T0":ot.Normal(500.2497543, 3.0),
-    "sinkQGSS.Q0":ot.Normal(183.713226, 0.75),
-    "sinkQGRE.Q0":ot.Normal(1922.331218, 8.5),
-}
-
-
-# Create a collection of the marginal distributions
+# Create the input probability distribution
 collectionMarginals = ot.DistributionCollection()
-# for name, distribution in dict_distribution:
 for distribution in dict_distribution.values():
     collectionMarginals.add(ot.Distribution(distribution))
 
-
-# Create the Spearman correlation matrix of the input random vector
-RS = ot.CorrelationMatrix(12)
-RS[4,6] = 0.0
-# Evaluate the correlation matrix of the Normal copula from RS
-R = ot.NormalCopula.GetCorrelationFromSpearmanCorrelation(RS)
-# Create the Normal copula parametrized by R
-copula = ot.NormalCopula(R)
-
-# Create the input probability distribution of dimension
-# inputDistribution = ot.ComposedDistribution(collectionMarginals,
-#                                             ot.Copula(copula))
-inputDistribution = ot.ComposedDistribution(collectionMarginals) #TMP
+inputDistribution = ot.ComposedDistribution(collectionMarginals)
 
 # Give a description of each component of the input distribution
 inputDistribution.setDescription(dict_distribution.keys())
@@ -142,15 +106,17 @@ def run_demo(with_initialization_script, seed=None, n_simulation=None):
     if seed is None:
         seed = 23091926
     if n_simulation is None:
-        # n_simulation = 100
-        n_simulation = 10 #TODO TMP
+        n_simulation = 100
 
     import time
     import numpy as np
 
     ot.RandomGenerator.SetSeed(seed)
 
+    outputs_fmu = ["BIL100.W_BIL100.signal"]
+    inputs_fmu = inputRandomVector.getDescription()
     model = instantiate_model(
+        inputs_fmu=inputs_fmu, outputs_fmu=outputs_fmu,
         with_initialization_script=with_initialization_script)
     # Create the output random vector
     outputRandomVector = ot.RandomVector(model, inputRandomVector)
@@ -189,7 +155,3 @@ if __name__ == "__main__":
     run_demo(with_initialization_script=False)
 
 #§
-# Local Variables:
-# tmux-temp-file: "/home/girard/.tmp/tmux_buffer"
-# tmux-repl-window: "fmot"
-# End:
