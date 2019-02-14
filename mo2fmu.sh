ee4feb5eef18beafa62c31b059bcc8c937fbf775
Julien Schueller <schueller@phimeca.com>
2019-02-14 10:06:34 +0100

    Fix temprorary files

--- mo2fmu.sh
+++ mo2fmu.sh
@@ -6,7 +6,7 @@ set -e
 
 usage()
 {
-  echo "Usage: ${0} source.mo [destination]"
+  echo "Usage: ${0} source.mo [destination.fmu]"
   exit 1
 }
 
@@ -17,17 +17,19 @@ mofile=`readlink -f ${1}`
 # assume the model name is the file name
 modelname=`basename ${mofile} | cut -d '.' -f 1`
 
-temp_dir=`mktemp -d` && cat > ${temp_dir}/mo2fmu.mos <<EOF
+if test $# -eq 2
+then
+  destination=`readlink -f ${2}`
+else
+  destination=${PWD}
+fi
+
+# export fmu in temp dir
+temp_dir=`mktemp -d` && cd ${temp_dir} && cat > mo2fmu.mos <<EOF
 loadFile("${mofile}"); getErrorString();
 translateModelFMU(${modelname}, version="2.0", fmuType="cs"); getErrorString();
 EOF
+omc mo2fmu.mos
 
-# output file in current dir or parent of destination
-if test $# -eq 2
-then
-  cd `dirname ${2}`
-fi
-
-omc ${temp_dir}/mo2fmu.mos
-
+mv ${modelname}.fmu ${destination}
 
