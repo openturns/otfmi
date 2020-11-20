ec6eb471ceb5ed30362ad112f5cf35cf7001eb74
Julien Schueller <schueller@phimeca.com>
2020-11-20 08:19:30 +0100

    Add poc

---
+++ poc/wrapper2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void c_func(int nin, double x[], int nout, double *y[])
+{
+  double E = x[0];
+  double F = x[1];
+  double L = x[2];
+  double I = x[3];
+//   for (int i = 0; i < 10; ++ i)
+//     y[i][0] = F*L*L*L/(3.0*E*I);
+}
+
