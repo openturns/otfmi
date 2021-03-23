021c1364d52123e5a3d07ab1ff86fe0cc923a4f4
Claire-Eleuthèriane Gerrer <40564064+Claire-Eleutheriane@users.noreply.github.com>
2021-03-23 09:55:11 +0100

    Test (#48)
    * Use metamodel as model or FMU in Modelica GUI
    
    * Update tests
    
    * Modify subprocess working dir
    
    * Correct path to cwrapper
    
    * [ongoing] debug
    
    * Fix bug in model copy
    
    * Adapt C library name to Windows platform

--- test/test_export.py
+++ test/test_export.py
@@ -6,24 +6,24 @@ import unittest
 import os
 import tempfile
 import sys
-import shutil
 import math as m
+import subprocess
 
 class TestExport(unittest.TestCase):
 
-    def test_export(self):
-        # create function
+    def test_export_fmu(self):
+        # export fmu
         f = ot.SymbolicFunction(['E', 'F', 'L', 'I'], ['(F*L^3)/(3.0*E*I)'])
         start = [3e7, 3e4, 250.0, 400.0]
 
         if sys.platform.startswith('win'):
             return
 
-        # export fmu
+
         temp_path = tempfile.mkdtemp()
-        path_fmu = os.path.join(temp_path, 'deviation.fmu')
+        path_fmu = os.path.join(temp_path, 'Deviation.fmu')
         fe = otfmi.FunctionExporter(f, start)
-        fe.export(path_fmu, fmuType='cs', verbose=True)
+        fe.export_fmu(path_fmu, fmuType='cs', verbose=False)
         assert os.path.isfile(path_fmu), "fmu not created"
 
         # reimport fmu
@@ -50,7 +50,7 @@ class TestExport(unittest.TestCase):
 
             # export
             fe = otfmi.FunctionExporter(f, start)
-            fe.export(path_fmu, fmuType='cs', verbose=True)
+            fe.export_fmu(path_fmu, fmuType='cs', verbose=True)
             assert os.path.isfile(path_fmu), "fmu not created"
 
             # import
@@ -61,7 +61,40 @@ class TestExport(unittest.TestCase):
             print(model.get_model_variables().keys())
             print(res['y0'])
 
-        shutil.rmtree(temp_path)
+        # shutil.rmtree(temp_path)
+
+    def test_export_model(self):
+    #     # export model
+        f = ot.SymbolicFunction(['E', 'F', 'L', 'I'], ['(F*L^3)/(3.0*E*I)'])
+        start = [3e7, 3e4, 250.0, 400.0]
+
+        temp_path = tempfile.mkdtemp()
+        assert os.path.isdir(temp_path), "temp_path not created"
+        name_model = 'Deviation.mo'
+        className, _ = os.path.splitext(name_model)
+        path_model = os.path.join(temp_path, name_model)
+        fe = otfmi.FunctionExporter(f, start)
+        fe.export_model(path_model, gui=False, verbose=False, move=True)
+        assert os.path.isfile(path_model), "model not created"
+
+        # write simulation mos
+        path_mos = os.path.join(temp_path, "simulate.mos")
+        with open(path_mos, "w") as mos:
+            mos.write('cd("{}");\n'.format(temp_path))
+            mos.write('loadModel(Modelica);\n')
+            mos.write('loadFile("{}"); getErrorString();\n'.format(
+                name_model))
+            mos.write('simulate ({}, stopTime=3.0);\n'.format(
+                className))
+
+        subprocess.run(['omc', '{}'.format(path_mos)], capture_output=True, check=True)
+
+        # shutil.rmtree(temp_path)
+
 
 if __name__ == '__main__':
     unittest.main()
+
+# note : the GUI model wrapping the OT object is not checked in these tests as
+# tests, as command line omc does not support the input/output connectors used
+# by OMEdit.
