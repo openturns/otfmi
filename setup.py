0cb78bf516ad9b1fd050e5d37e89ec44ef72a783
Julien Schueller <schueller@phimeca.com>
2021-04-21 17:46:03 +0200

    Add mo2fmu entry point

--- setup.py
+++ setup.py
@@ -96,9 +96,9 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-        # 'console_scripts': [
-            # 'sample=sample:main',
-        # ],
-    # },
+    entry_points={
+        'console_scripts': [
+            'mo2fmu = otfmi.mo2fmu:main',
+        ],
+    },
 )
