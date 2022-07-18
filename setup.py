0bedc333496c5311d3601e486f98480ed65b36cf
Julien Schueller <schueller@phimeca.com>
2022-07-18 16:22:12 +0200

    FunctionExporter: Use jinja templating

--- setup.py
+++ setup.py
@@ -64,7 +64,8 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['numpy',
                       'pyfmi>=2.6',
-                      'dill'],
+                      'dill',
+                      'jinja2'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
