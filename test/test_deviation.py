42c41c2a144ebc599daae6f116ef6a79141d0e98
Julien Schueller <schueller@phimeca.com>
2023-11-17 15:53:23 +0100

    Testing: Drop unittest

--- test/test_deviation.py
+++ test/test_deviation.py
@@ -1,54 +1,32 @@
-import unittest
 import openturns as ot
 import otfmi
-import otfmi.example
+import otfmi.example.utility
 import otfmi.example.deviation
+import sys
 
 
-class TestModel(unittest.TestCase):
-    def setUp(self):
-        """Load FMU and setup pure python reference."""
+def test_deviation():
+    model_py = ot.PythonFunction(
+        4, 1, otfmi.example.deviation.deviationFunction
+    )
 
-        self.model_py = ot.PythonFunction(
-            4, 1, otfmi.example.deviation.deviationFunction
-        )
+    # FMU model
+    path_fmu = path_fmu = otfmi.example.utility.get_path_fmu("deviation")
+    model_fmu = otfmi.FMUFunction(
+        path_fmu, inputs_fmu=["E", "F", "L", "I"], outputs_fmu="y"
+    )
 
-        # FMU model
-        path_fmu = path_fmu = otfmi.example.utility.get_path_fmu("deviation")
-        self.model_fmu = otfmi.FMUFunction(
-            path_fmu, inputs_fmu=["E", "F", "L", "I"], outputs_fmu="y"
-        )
+    seed = 23091926
+    coefficient_variation = 1.0
+    ot.RandomGenerator.SetSeed(seed)
+    probability_py = otfmi.example.deviation.run_monte_carlo(
+        model_py, coefficient_variation=coefficient_variation
+    )
 
-    # def tearDown(self):
-    # pass
+    ot.RandomGenerator.SetSeed(seed)
+    probability_fmu = otfmi.example.deviation.run_monte_carlo(
+        model_fmu, coefficient_variation=coefficient_variation
+    )
 
-    def test_empty(self):
-        """Check module import and setup."""
-        pass
-
-    def test_compare_fmu_pure_python(self):
-        """Compare the threshold exceedance probability computed with the FMU
-        and the pure python reference.
-        """
-
-        seed = 23091926
-        coefficient_variation = 1.0
-        ot.RandomGenerator.SetSeed(seed)
-        probability_py = otfmi.example.deviation.run_monte_carlo(
-            self.model_py, coefficient_variation=coefficient_variation
-        )
-
-        ot.RandomGenerator.SetSeed(seed)
-        probability_fmu = otfmi.example.deviation.run_monte_carlo(
-            self.model_fmu, coefficient_variation=coefficient_variation
-        )
-
-        relative_error = abs(probability_py - probability_fmu) / probability_py
-
-        from numpy import finfo
-
-        assert relative_error < finfo(float).eps
-
-
-if __name__ == "__main__":
-    unittest.main()
+    relative_error = abs(probability_py - probability_fmu) / probability_py
+    assert relative_error < sys.float_info.epsilon
