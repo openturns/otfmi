b6b12771cb1233ede5809af0c7bc53bc3019a2e8
Julien Schueller <schueller@phimeca.com>
2023-11-21 16:51:52 +0100

    Drop unitest

--- test/test_epid.py
+++ test/test_epid.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import unittest
 import otfmi
 import otfmi.example.deviation
 import otfmi.example.utility
 import pyfmi
+import pytest
+
 
 input_value = [0.007, 0.02]
 
@@ -27,61 +28,57 @@ def simulate_with_pyfmi(path_fmu, final_time=None):
     return list_last_infected_value
 
 
-class TestEpid(unittest.TestCase):
-    def setUp(self):
-        """Load FMU and setup pure python reference."""
-        self.path_fmu = otfmi.example.utility.get_path_fmu("epid")
-        self.model_fmu = otfmi.FMUFunction(
-            self.path_fmu,
-            inputs_fmu=["infection_rate", "healing_rate"],
-            outputs_fmu=["infected"],
-        )
-
-    def test_empty(self):
-        """Check module import and setup."""
-        pass
-
-    def test_final_value(self):
-        """Check reproducibility of the final value."""
-        y = self.model_fmu(input_value)
-        assert abs(y[0] - 265.68) < 1e-2, "wrong value"
-
-    def test_fmufunction_interpolation(self):
-        """Check that FMUFunction returns a point corresponding to Pyfmi's
-        interpolated output.
-        """
-        list_last_infected_value = simulate_with_pyfmi(self.path_fmu)
-        y = self.model_fmu(input_value)
-        assert y[0] < max(list_last_infected_value)
-        assert y[0] > min(list_last_infected_value)
-
-    def test_final_time(self):
-        """Check that specified final time is taken into account."""
-        final_time = 2.0
-        model_fmu_1 = otfmi.FMUFunction(
-            self.path_fmu,
-            inputs_fmu=["infection_rate", "healing_rate"],
-            outputs_fmu=["infected"],
-            final_time=final_time,
-        )
-        list_last_infected_value = simulate_with_pyfmi(
-            self.path_fmu, final_time=final_time
-        )
-        y = model_fmu_1(input_value)
-        assert y[0] < max(list_last_infected_value)
-        assert y[0] > min(list_last_infected_value)
-
-    def test_keyword(self):
-        """Check that specifying final time at simulation raises a Warning."""
-        lowlevel_model_fmu = otfmi.OpenTURNSFMUFunction(
-            self.path_fmu,
-            inputs_fmu=["infection_rate", "healing_rate"],
-            outputs_fmu=["infected"],
-        )
-        self.assertRaises(
-            Warning, lowlevel_model_fmu._exec, input_value, final_time=50.0
-        )
+@pytest.fixture
+def path_fmu():
+    return otfmi.example.utility.get_path_fmu("epid")
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def model_fmu(path_fmu):
+    return otfmi.FMUFunction(path_fmu,
+                             inputs_fmu=["infection_rate", "healing_rate"],
+                             outputs_fmu=["infected"])
+
+
+def test_final_value(model_fmu):
+    """Check reproducibility of the final value."""
+    y = model_fmu(input_value)
+    assert abs(y[0] - 265.68) < 1e-2, "wrong value"
+
+
+def test_fmufunction_interpolation(path_fmu, model_fmu):
+    """Check that FMUFunction returns a point corresponding to Pyfmi's
+    interpolated output.
+    """
+    list_last_infected_value = simulate_with_pyfmi(path_fmu)
+    y = model_fmu(input_value)
+    assert y[0] < max(list_last_infected_value)
+    assert y[0] > min(list_last_infected_value)
+
+
+def test_final_time(path_fmu):
+    """Check that specified final time is taken into account."""
+    final_time = 2.0
+    model_fmu_1 = otfmi.FMUFunction(
+        path_fmu,
+        inputs_fmu=["infection_rate", "healing_rate"],
+        outputs_fmu=["infected"],
+        final_time=final_time,
+    )
+    list_last_infected_value = simulate_with_pyfmi(
+        path_fmu, final_time=final_time
+    )
+    y = model_fmu_1(input_value)
+    assert y[0] < max(list_last_infected_value)
+    assert y[0] > min(list_last_infected_value)
+
+
+def test_keyword(path_fmu):
+    """Check that specifying final time at simulation raises a Warning."""
+    lowlevel_model_fmu = otfmi.OpenTURNSFMUFunction(
+        path_fmu,
+        inputs_fmu=["infection_rate", "healing_rate"],
+        outputs_fmu=["infected"],
+    )
+    with pytest.raises(Warning):
+        lowlevel_model_fmu._exec(input_value, final_time=50.0)
