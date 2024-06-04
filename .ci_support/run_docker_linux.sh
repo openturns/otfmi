f316f585637783554624f4122da1a4278bac6415
Julien Schueller <schueller@phimeca.com>
2024-06-04 21:20:59 +0200

    CI: Regenerate DeviationSlave.fmu

--- .ci_support/run_docker_linux.sh
+++ .ci_support/run_docker_linux.sh
@@ -10,6 +10,8 @@ cd /tmp
 pip install . --user --break-system-packages
 ~/.local/bin/mo2fmu -h
 ~/.local/bin/mo2fmu ./otfmi/example/file/deviation.mo ./otfmi/example/file/fmu/linux-x86_64/deviation.fmu
+pythonfmu build --file ./otfmi/example/file/DeviationSlave.py --dest ./otfmi/example/file/fmu/linux-x86_64
+
 pytest test -s
 cd doc && make html BUILDDIR=~/.local/share/otfmi/doc
 
