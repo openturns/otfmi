6a1b0999581353e22153642e10a48a8d641b1686
Sylvain Girard <girard@phimeca.com>
2024-11-08 16:21:24 +0100

    --

--- test/test_epid.py
+++ test/test_epid.py
@@ -11,6 +11,7 @@ import pytest
 input_value = [2.0, 0.5]
 final_time = 5
 
+
 def simulate_with_pyfmi(path_fmu, final_time=None):
     """Return the output on the 5 last simulation times.
     By default, pyfmi simulation output counts 500 steps.
@@ -40,10 +41,12 @@ def model_fmu(path_fmu):
                              inputs_fmu=["infection_rate", "healing_rate"],
                              outputs_fmu=["infected"], final_time=final_time)
 
+
 def test_final_value(model_fmu):
     """Check reproducibility of the final value."""
     y = model_fmu(input_value)
     assert abs(y[0] - 303.785) < 1e-2, "wrong value"
+
 
 def test_final_time(path_fmu):
     """Check that specified final time is taken into account."""
@@ -58,8 +61,7 @@ def test_final_time(path_fmu):
         path_fmu, final_time=final_time
     )
     y = model_fmu_1(input_value)
-    assert abs(y[0] - list_last_infected_value[-1]) <1e-5
-
+    assert abs(y[0] - list_last_infected_value[-1]) < 1e-5
 
 
 def test_keyword(path_fmu):
