45d25596b2fb641ac0183ca8e1734017b8e2a460
Julien Schueller <schueller@phimeca.com>
2025-10-29 18:19:17 +0100

    Add FMUFieldToPointFunction

--- otfmi/__init__.py
+++ otfmi/__init__.py
@@ -5,9 +5,13 @@ from .otfmi import (
     OpenTURNSFMUFunction,
     FMUPointToFieldFunction,
     OpenTURNSFMUPointToFieldFunction,
+    FMUFieldToPointFunction,
+    OpenTURNSFMUFieldToPointFunction,
 )
 from .function_exporter import FunctionExporter
 from .mo2fmu import mo2fmu
 
-__all__ = [FMUFunction, OpenTURNSFMUFunction, FMUPointToFieldFunction,
-           OpenTURNSFMUPointToFieldFunction, FunctionExporter, mo2fmu]
+__all__ = [FMUFunction, OpenTURNSFMUFunction,
+           FMUPointToFieldFunction, OpenTURNSFMUPointToFieldFunction,
+           FMUFieldToPointFunction, OpenTURNSFMUFieldToPointFunction,
+           FunctionExporter, mo2fmu]
