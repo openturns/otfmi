d132e230f792f428d4f35170d4a25bceeecf6151
blondet <blondet@phimeca.com>
2026-01-30 16:05:21 +0100

    clean config script

--- doc/conf.py
+++ doc/conf.py
@@ -18,10 +18,7 @@ sys.path.insert(0, os.path.abspath("../"))
 
 extensions = [
     "sphinx.ext.autodoc",
-    #"sphinx.ext.extlinks",
     "sphinx.ext.intersphinx",
-    #"sphinx.ext.mathjax",
-    #"sphinx.ext.todo",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
     "numpydoc",
