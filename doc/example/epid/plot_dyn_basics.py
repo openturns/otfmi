bbee56895d2368c7243970e87a4eb99970b2b614
Julien Schueller <schueller@phimeca.com>
2026-02-24 19:06:09 +0100

    Doc: Improve epid example plot
    Closes #117

--- doc/example/epid/plot_dyn_basics.py
+++ doc/example/epid/plot_dyn_basics.py
@@ -11,38 +11,23 @@ FMUPointToFieldFunction basics
 # :py:class:`openturns.Sample` or a set of :py:class:`openturns.Field`.
 
 # %%
-# First, retrieve the path to *epid.fmu*.
 import openturns as ot
 import otfmi.example.utility
-import matplotlib.pyplot as plt
 import openturns.viewer as viewer
 
+# %%
+# First, retrieve the path to *epid.fmu*.
 path_fmu = otfmi.example.utility.get_path_fmu("epid")
 
 # %%
-# Define the time grid for the FMU's output. The last value of the time grid,
-# here 10., will define the FMU stop time for simulation.
-mesh = ot.RegularGrid(0.0, 0.1, 2000)
-meshSample = mesh.getVertices()
-print(meshSample)
-
-# %%
-# .. note::
-#    The FMU solver uses its own time grid for simulation.
-#    The FMU output is then interpolated on the user-provided time grid.
-
-# %%
 # Wrap the FMU in an :py:class:`openturns.PointToFieldFunction` object:
-
 function = otfmi.FMUPointToFieldFunction(
     path_fmu,
-    mesh,
     inputs_fmu=["infection_rate"],
-    outputs_fmu=["infected"],
+    outputs_fmu=["infected", "susceptible", "removed"],
     start_time=0.0,
-    final_time=200.0,
+    final_time=15.0,
 )
-print(type(function))
 
 # %%
 # .. note::
@@ -52,32 +37,43 @@ print(type(function))
 
 # %%
 # Simulate the function on an input :py:class:`openturns.Point` yields an output
-# :py:class:`openturns.Sample`, corresponding to the output evolution over time:
+# as :py:class:`openturns.Sample`, corresponding to the output evolution over time
+x = [2.0]
+y = function(x)
 
-inputPoint = ot.Point([2.0])
-outputSample = function(inputPoint)
+# %%
+# Retrieve the simulation mesh
+timeGrid = function.getOutputMesh().getVertices()
 
-plt.xlabel("FMU simulation time (s)")
-plt.ylabel("Number of Infected")
-plt.plot(meshSample, outputSample)
-plt.show()
+# %%
+# Plot the data
+graph = ot.Graph("Simulation data", "Simulation time (days)", "Number of individuals", True)
+graph.setLegendPosition("upper right")
+for i in range(function.getOutputDimension()):
+    curve = ot.Curve(timeGrid, y[:, i])
+    graph.add(curve)
+graph.setLegends(function.getOutputDescription())
+graph.setColors(["red", "blue", "green"])
+view = viewer.View(graph)
 
 # %%
 # Simulate the function on a input :py:class:`openturns.Sample` yields a set of
 # fields called :py:class:`openturns.ProcessSample`:
-
-inputSample = ot.Sample([[2.0], [2.25], [2.5]])
-outputProcessSample = function(inputSample)
-print(outputProcessSample)
+X = [[2.0], [2.25], [2.5]]
+Y = function(X)
+print(Y)
 
 # %%
 # Visualize the time evolution of the ``infected`` over time, depending on the
 # `ìnfection_rate`` value:
-gridLayout = outputProcessSample.draw()
+gridLayout = Y.draw()
 graph = gridLayout.getGraph(0, 0)
 graph.setTitle("")
-graph.setXTitle("FMU simulation time (s)")
-graph.setYTitle("Number of infected")
-graph.setLegends([str(line[0]) for line in inputSample])
+graph.setXTitle("Simulation time (days)")
+graph.setYTitle(function.getOutputDescription()[0])
+graph.setLegends([str(line[0]) for line in X])
 view = viewer.View(graph, legend_kw={"title": "infection rate"})
+
+# %%
+# Show all plots
 view.ShowAll()
