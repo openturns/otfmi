e62a8602d8eb46a53d542a6767ef0de89c224391
Julien Schueller <schueller@phimeca.com>
2026-03-07 12:39:49 +0100

    FMUBaseFunction: Check input causality
    Closes #148

--- .ci_support/run_docker_linux.sh
+++ .ci_support/run_docker_linux.sh
@@ -9,7 +9,6 @@ cp -r /io/* /tmp
 cd /tmp
 pip install . --user --break-system-packages --no-deps
 ~/.local/bin/mo2fmu -h
-~/.local/bin/mo2fmu ./otfmi/example/file/deviation.mo ./otfmi/example/file/fmu/linux-x86_64/deviation.fmu
 pythonfmu build --file ./otfmi/example/file/DeviationSlave.py --dest ./otfmi/example/file/fmu/linux-x86_64
 
 pytest test -s
