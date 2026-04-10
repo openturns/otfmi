16956d7cc28f3edcaa6b046eb1a6086250945aff
Julien Schueller <schueller@phimeca.com>
2026-04-10 13:54:22 +0200

    Doc: Fix epid example
    Closes #117

--- doc/examples/p2f/epid/plot_dyn_set.py
+++ doc/examples/p2f/epid/plot_dyn_set.py
@@ -5,11 +5,10 @@ Set FMU simulation parameters
 
 # %%
 # :class:`~otfmi.FMUPointToFieldFunction` is an OpenTURNS-friendly overlay of the class
-# :py:class:`OpenTURNSFMUPointToFieldFunction`, closer to the underlying PyFMI
+# otfmi.OpenTURNSFMUPointToFieldFunction, closer to the underlying PyFMI
 # implementation.
 # Some FMU simulation parameters can be given to :class:`~otfmi.FMUPointToFieldFunction`,
-# yet most of them can only be passed to an
-# :py:class:`OpenTURNSFMUPointToFieldFunction`.
+# yet most of them can only be passed to an OpenTURNSFMUPointToFieldFunction.
 
 # %%
 # First, retrieve the path to *epid.fmu*.
@@ -22,15 +21,12 @@ path_fmu = otfmi.example.utility.get_path_fmu("epid")
 # The FMU simulation start and final times are the only simulation-related
 # parameter that can be passed to :class:`~otfmi.FMUPointToFieldFunction`.
 
-mesh = ot.RegularGrid(0.0, 0.1, 20)
-
 function = otfmi.FMUPointToFieldFunction(
     path_fmu,
-    mesh,
     inputs_fmu=["infection_rate", "healing_rate"],
     outputs_fmu=["infected"],
     start_time=0.0,
-    final_time=2.0,
+    final_time=15.0,
 )
 
 inputPoint = ot.Point([0.007, 0.02])
@@ -39,16 +35,15 @@ print(outputSample)
 
 # %%
 # To set more parameters for the FMU simulation,
-# :py:class:`OpenTURNSFMUPointToFieldFunction` can be employed. Below, we set the PyFMI
+# OpenTURNSFMUPointToFieldFunction can be employed. Below, we set the PyFMI
 # algorithm running the simulation, and require simulation silent mode.
 
 midlevel_function = otfmi.OpenTURNSFMUPointToFieldFunction(
     path_fmu,
-    mesh,
     inputs_fmu=["infection_rate", "healing_rate"],
     outputs_fmu=["infected"],
     start_time=0.0,
-    final_time=2.0,
+    final_time=15.0,
 )
 
 outputPoint = midlevel_function.base.simulate(
@@ -56,7 +51,7 @@ outputPoint = midlevel_function.base.simulate(
 )
 
 # %%
-# For advanced users, the middle-level class :py:class:`OpenTURNSFMUPointToFieldFunction` also gives
+# For advanced users, the middle-level class otfmi.OpenTURNSFMUPointToFieldFunction also gives
 # access to the PyFMI model. We can hence access all PyFMI's object methods:
 
 pyfmi_model = midlevel_function.base.get_model()
@@ -64,5 +59,5 @@ print(dir(pyfmi_model))
 
 # %%
 # .. note::
-#    otfmi' classes :class:`~otfmi.FMUPointToFieldFunction` and :py:class:`OpenTURNSFMUPointToFieldFunction`
+#    otfmi' classes :class:`~otfmi.FMUPointToFieldFunction` and otfmi.OpenTURNSFMUPointToFieldFunction
 #    are designed to highlight the most useful PyFMI's methods and simplify their use!
